20540a5ac215dc45cf85ae6d0034f44d35362a12
Arya <arya.torabi2054@gmail.com>
2025-07-13 07:07:38 +0330

    Deploy website - based on ad6435a690412b28a5767a56f4a602f83190c231

---
+++ assets/js/acecf23e.5970c6b3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/senfi.sharif.ir/blog","blogTitle":"Blog","authorsListPath":"/senfi.sharif.ir/blog/authors"}')}}]);
---
+++ assets/js/b5fd9b06.c67b1477.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7397],{5703:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/senfi.sharif.ir/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
